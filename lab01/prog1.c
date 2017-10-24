21436ad7afe81949a0455739e415f72cdaabff24
Krystix13 <krystek1397@gmail.com>
2017-10-24 20:32:19 +0200

    program 1

--- lab01/prog1.c
+++ lab01/prog1.c
@@ -1,4 +1,13 @@
 #include <stdio.h>
 
 int main(){
+	int n;
+	int sum=0;
+	printf("Podaj liczbe calkowita: ");
+	scanf("%d", &n);
+	if(n<0) n = n * (-1);
+	for(int i=1; i<=n; i++){
+		sum += i;
+	} 
+	printf("%s%d%s%d\n" , "Suma od 1 do " , n , " jest rowna " , sum);
 }
