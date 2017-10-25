e2a045c64856fc80fdaf6bf10bce1f974d94467b
Krystix13 <krystek1397@gmail.com>
2017-10-25 16:41:56 +0200

    update prog17

--- lab01/prog17.c
+++ lab01/prog17.c
@@ -8,6 +8,10 @@ int main(){
 		printf("Liczba nie jest pierwsza!\n");
 		return 0;
 	}
+	if(n==2|| n==3){
+		printf("Liczba jest pierwsza :).");
+		return 0;
+	}
 	 for(int i=2;i*i<=n;i++){
 		if(n%i==0){
 			printf("Liczba nie jest pierwsza!\n");
