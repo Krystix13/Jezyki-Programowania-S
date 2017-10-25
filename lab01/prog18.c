b78dd083e0fe2dab79f5508ebabbdeb26e24347b
Krystix13 <krystek1397@gmail.com>
2017-10-25 20:22:58 +0200

    update prog 18

--- lab01/prog18.c
+++ lab01/prog18.c
@@ -1,48 +1,41 @@
 #include <stdio.h>
+#include <math.h>
+
+int pierwsza(int a){
+	int k;
+	k=sqrt(a);
+	for(int i = 2; i<=k; i++){
+		if(a%i==0)
+		return 0;
+	}
+	if(a==1 || a==0)
+		return 0;
+	return 1;
+}
+
+int suma(int a){
+	int wynik = 0;
+	for(;a!=0 ;){
+		wynik += a%10;
+		a /= 10;
+	}
+	return wynik;
+}
 
 int main(){
-	
-	int n,buf;
+	int n;
+	int s = 0;
 	int licznik = 0;
-	int sum = 0;
-	int suml= 0;
-	int ilosc = 0;
-	printf("Podaj ile liczb z suma cyfr 10 ma zsumowac: \n");
-	scanf("%d", &n);
 	
-	for(int i = 1; i<100000000; i++){
-		buf = i;
-		while(buf>0){
-			buf = buf/10;
-			ilosc++;
+	printf("Ile liczb z pierwsza suma cyfr ma zsumowac: \n");
+	scanf("%d" , &n);
+	
+	for(int i = 0; licznik < n; i++){
+		if(pierwsza(suma(i))){
+			licznik++;
+			s += i;
 		}
-		buf = i;
-		for(int j = 0; j<ilosc; j++){
-			sum += buf%10;
-			buf = buf/10;
-			if(sum==2 || sum==3){
-				suml +=i;
-				continue;
-			}
-			if(sum<2){
-				continue;
-			}
-			else{
-				for(int k=2;k*k<=i;k++){
-					if(i%k==0){
-					//liczba nie jest pierwsza
-					break;
-					}
-					suml += i;
-					licznik++;	
-				}
-			}
-			if(licznik == n){
-				printf("%s%d%s%d\n", "Suma " , n ," pierwszych liczb z suma cyfr 10 wynosi: " , suml);
-				return 0;
-			}
-		}
-		ilosc = 0;
-		sum = 0;
 	}
+	
+	printf("%s%d\n", "Suma liczb: " , s);
 }
