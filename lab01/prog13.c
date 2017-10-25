fc36046703076064ac3b8d05fd1c3de6841c1605
Krystix13 <krystek1397@gmail.com>
2017-10-25 14:29:58 +0200

    prog 13

--- lab01/prog13.c
+++ lab01/prog13.c
@@ -1,8 +1,23 @@
 #include <stdio.h>
 
 int main(){
-	int l;
-	fprint("Podaj liczbe: ");
+	int l,buf;
+	int ilosc = 0;
+	printf("Podaj liczbe: ");
 	scanf("%d" , &l);
-	
+	buf = l;
+	while(buf>0){
+		buf = buf/10;
+		ilosc++;
+	}
+	for(int i = 0; i<ilosc; i++){
+		if(l%2==0) printf("Jest parzysta\n");
+		else{
+			printf("Cyfra jest nieparzysta\n");
+			return 0;
+		}
+		l = l/10;
+	}
+	printf("Cyfry sa parzyste.\n");
+	return 0;
 }
