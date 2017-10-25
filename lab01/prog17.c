0722896b2ddfa34fc664d2dc1d1b73b334503870
Krystix13 <krystek1397@gmail.com>
2017-10-25 15:44:36 +0200

    prog 17

---
+++ lab01/prog17.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+	int n;
+	printf("Podaj liczbe do sprawdzenia: \n");
+	scanf("%d" , &n);
+	if(n<2){
+		printf("Liczba nie jest pierwsza!\n");
+		return 0;
+	}
+	 for(int i=2;i*i<=n;i++){
+		if(n%i==0){
+			printf("Liczba nie jest pierwsza!\n");
+			return 0;
+		}
+	printf("Liczba jest pierwsza :).\n");
+	return 0;
+	 }
+}
